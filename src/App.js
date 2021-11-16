229ddb9350e91a79d25e07022975cf0293cf6237
Victor Teixeira <victor@donit.io>
2021-11-16 05:33:30 -0800

    Change App

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Victor
+          Edit <code>src/App.js</code> and save to reload. Victor2
         </p>
         <a
           className="App-link"
